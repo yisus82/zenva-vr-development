323274a3ebac252a1ef315eb3123f8b977ab9d56
Jesús Ángel Pérez-Roca Fernández <yisus82@users.noreply.github.com>
2019-11-05 13:18:13 +0100

    Models and platform movement

---
+++ FlyingPlatform/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 1;
+    private bool isMoving = false;
+
+    private void Update()
+    {
+        HandleInput();
+        HandleMovement();
+
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            isMoving = !isMoving;
+        }
+    }
+
+    private void HandleMovement()
+    {
+        if (!isMoving)
+        {
+            return;
+        }
+
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if (distance > 0)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+    }
+}
